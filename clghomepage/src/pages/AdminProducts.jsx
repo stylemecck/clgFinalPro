1ab796e758a50c686c31a646b3304cc30b70e150
stylemecck <satyamkashyapoffical@gmail.com>
2025-04-18 17:43:58 +0530

    upadte in product image

--- clghomepage/src/pages/AdminProducts.jsx
+++ clghomepage/src/pages/AdminProducts.jsx
@@ -178,7 +178,9 @@ const AdminProducts = () => {
                   <tr key={product.id} className="hover:bg-gray-50">
                     <td className="px-6 py-4 whitespace-nowrap">
                       <div className="flex items-center">
-                        <div className="flex-shrink-0 h-10 w-10 bg-gray-200 rounded-lg"></div>
+                        <div className="flex-shrink-0 h-10 w-10 bg-gray-200 rounded-lg">
+                            {/* add product image  */}
+                        </div>
                         <div className="ml-4">
                           <div className="text-sm font-medium text-gray-900">{product.name}</div>
                         </div>
