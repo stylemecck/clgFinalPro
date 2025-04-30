5608d14c0a59f58f3f2f66cfdffac02a29546d1d
stylemecck <satyamkashyapoffical@gmail.com>
2025-04-30 21:22:10 +0530

    update in slider

--- clghomepage/src/components/FlipkartSlider.jsx
+++ clghomepage/src/components/FlipkartSlider.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectFade, Pagination } from "swiper/modules";
 import "swiper/css";
@@ -6,39 +7,45 @@ import "swiper/css/navigation";
 import "swiper/css/effect-fade";
 import "swiper/css/pagination";
 
-const FlipkartSlider = ({ images = [] }) => {
+const FlipkartSlider = ({ images = [], links = [] }) => {
   return (
-    <Swiper
-      modules={[Autoplay, EffectFade, Pagination]}
-      autoplay={{
-        delay: 4000,
-        disableOnInteraction: false,
-      }}
-      effect="fade"
-      fadeEffect={{ crossFade: true }}
-      loop={true}
-      speed={800}
-      pagination={{
-        clickable: true,
-        el: ".custom-pagination",
-        renderBullet: (index, className) =>
-          `<span class="${className} custom-bullet"></span>`,
-      }}
-      className="w-[98vw] h-[30vh] sm:h-[70vh] md:h-[80vh] lg:h-[90vh] mt-2 relative"
-    >
-      {images.map((img, index) => (
-        <SwiperSlide key={index}>
-          <div className="w-full h-full relative">
-            <img
-              src={img}
-              alt={`Slide ${index + 1}`}
-              className="w-full h-full object-fill rounded-lg sm:rounded-xl"
-            />
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="relative w-[98vw] mt-2 mx-3.5">
+      <Swiper
+        modules={[Autoplay, EffectFade, Pagination]}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        effect="fade"
+        fadeEffect={{ crossFade: true }}
+        loop={true}
+        speed={800}
+        pagination={{
+          clickable: true,
+          el: ".custom-pagination",
+          renderBullet: (index, className) =>
+            `<span class="${className} custom-bullet"></span>`,
+        }}
+        className="w-[98vw] h-[30vh] sm:h-[70vh] md:h-[80vh] lg:h-[90vh] mt-2 relative"
+      >
+        {images.map((img, index) => (
+          <SwiperSlide key={index}>
+            <div className="w-full h-full relative">
+              <Link to={links[index] || "#"}>
+                <img
+                  src={img}
+                  alt={`Slide ${index + 1}`}
+                  className="w-full h-full object-fill rounded-lg sm:rounded-xl"
+                />
+              </Link>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="custom-pagination absolute bottom-4 left-0 right-0 flex justify-center z-10" />
+    </div>
   );
 };
 
 export default FlipkartSlider;
+//ishme kuch nahi karna hai  sidha ek components bana ke copy paste
--- clghomepage/src/pages/HomePage.jsx
+++ clghomepage/src/pages/HomePage.jsx
@@ -1,26 +1,25 @@
 // pages/HomePage.jsx
-import React from 'react';
-import Slider from '../components/Slider';
-import NewsLatter from '../components/NewsLatter';
-import TrendingProducts from '../components/TrendingProducts';
-import Categories from '../components/Categories';
-import PromoBanner from '../components/PromoBanner';
-import Nav from '../components/navbar/Nav';
-import Navt from '../components/navbar/Navt';
-import FlipkartSlider from '../components/FlipkartSlider';
-import Navbar from './Navbar';
+import React from "react";
+import NewsLatter from "../components/NewsLatter";
+import TrendingProducts from "../components/TrendingProducts";
+import Categories from "../components/Categories";
+import PromoBanner from "../components/PromoBanner";
+import FlipkartSlider from "../components/FlipkartSlider";
 
-import slideimg1 from "../assets/banner/bw1.gif";
+
+import slideimg1 from "../assets/banner/bw1.gif"; // image ko import kar lena
 import slideimg2 from "../assets/banner/bw2.gif";
-import slideimg3 from "../assets/banner/bm1.gif";
+import slideimg3 from "../assets/banner/bm1.gif"; // kahi pe bhi rakho bas yeha pe import kar lena kaam karega chaho toh public wala me bhi rakh sakte ho ya apna se folder structure bana ke 
 
 const HomePage = () => {
 
-    const bannerImages = [slideimg1, slideimg2, slideimg3];
-
   return (
     <>
-      <FlipkartSlider images={bannerImages} />
+      <FlipkartSlider
+        images={[slideimg1, slideimg2, slideimg3]} //props ke through image 
+        links={["/product/1", "/product/2", "/login"]} // yeha pe link array container hai
+      />
+
       <Categories />
       <TrendingProducts />
       <PromoBanner />
